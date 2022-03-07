1619ce30b6d5aa671137c4fa9a1705d2aedbcc21
dimitris chortarias <dimitris.chortarias@gmail.com>
2022-03-07 18:44:53 +0200

    d

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import pandas as pd
-import numpy as np
-import fastf1
-import seaborn as sns
-import matplotlib.pyplot as plt
 import streamlit as st
-
-sns.set()
-
-fastf1.Cache.enable_cache(r"C:\Cachef1")  # replace with your cache directory
+import pandas as pd
+from tyre_analysis import create_race_data
 
 st.set_page_config(
     page_title="Formula 1 Data Analysis",
@@ -17,141 +10,17 @@ st.set_page_config(
     menu_items=None,
 )
 st.title("Formula 1 Tyre Analysis")
-i = 1
 
-# array(['Alpine F1 Team', 'Mercedes', 'AlphaTauri', 'Alfa Romeo',
-#        'Williams', 'Ferrari', 'Haas F1 Team', 'McLaren', 'Red Bull',
-#        'Aston Martin'], dtype=object)
-# List of Drivers ['VER', 'HAM', 'BOT', 'LEC', 'GAS', 'RIC', 'NOR', 'SAI', 'ALO',
-#        'STR', 'PER', 'GIO', 'TSU', 'RAI', 'RUS', 'OCO', 'LAT', 'MSC',
-#        'MAZ', 'VET']
-# Running for each session
+# data.to_pickle('data.pkl')
+# data = create_race_data()
+data = pd.read_pickle("data.pkl")
+Driver = data["Driver"].unique()
+# Team = data["Team"].unique()
+Race = data["Race"].unique()
 
-
-@st.cache()
-def load_race_data():
-    df = pd.DataFrame()
-    count = 1
-    while count <= 22:
-        race = fastf1.get_session(2021, count, "R")
-        laps = race.load_laps(with_telemetry=True)
-        laps["Race"] = race.weekend.name
-        df = pd.concat([df, laps])
-        count += 1
-    return df
+race_choice = st.sidebar.selectbox("Select Race", Race)
+# team_choice = st.sidebar.selectbox("Select Team", Team)
+driver_choice = st.sidebar.selectbox("Select Driver:", Driver)
 
 
-data = load_race_data()
-
-df = pd.DataFrame()
-
-
-def remove_wet_races(data):
-    wet_races = (
-        data[["Race", "Compound", "LapTime"]]
-        .groupby(["Race", "Compound"])
-        .count()
-        .reset_index()
-    )
-    wet_races = pd.pivot_table(
-        wet_races, index="Race", columns="Compound", values="LapTime"
-    ).reset_index()
-    wet_races = wet_races.fillna(0)
-    wet_races = wet_races[(wet_races["WET"] == 0) & (wet_races["INTERMEDIATE"] == 0)]
-    data = pd.merge(data, wet_races[["Race"]], on="Race")
-    return data
-
-
-def add_difference_from_the_car_in_front(data):
-    # Sort Values by Following Car
-    data = data.sort_values(by=["Race", "Time"]).reset_index(drop=True)
-    ##Add column with the delta between 2 cars in the beginning of the lap
-    data["followingcar"] = data["Time"] - data["Time"].shift(1)
-    return data
-
-
-def filter_dataframe(data):
-    # Filter Dataframe where
-    # 1.there is no pit in or pit out in this lap
-    # 2.The track is Clear
-    # 3.there is no car ahead less than 1 second
-    # 4.Only Dry Tyres
-    # 5.Keeping only necessery Columns
-    data = data[
-        (data["PitOutTime"].isnull())
-        & (data["PitInTime"].isnull())
-        & (data["TrackStatus"] == "1")
-        & (data["followingcar"] > pd.Timedelta(1.1, unit="s"))
-    ][
-        [
-            "LapTime",
-            "LapNumber",
-            "Compound",
-            "TyreLife",
-            "Team",
-            "Driver",
-        ]
-    ]
-    return data
-
-
-# Creating a column with the tyre delta between the laps
-laps = laps.sort_values(by=["Driver", "LapNumber", "TyreLife", "Compound"]).reset_index(
-    drop=True
-)
-laps["tyredelta"] = np.where(
-    (laps["Driver"] == laps["Driver"].shift(1))
-    & (laps["Compound"] == laps["Compound"].shift(1)),
-    pd.to_timedelta(laps["LapTime"] - laps["LapTime"].shift(1)),
-    pd.NaT,
-)
-laps["tyredelta"] = laps["tyredelta"].fillna(9999 * 1000)
-laps["tyredelta"] = (laps["tyredelta"] / 1000).astype("int")
-
-# Drop rows where there are containing na values
-laps = laps.dropna()
-
-# Calculating Lap Times in Seconds
-laps["lapinseconds"] = laps["LapTime"] / np.timedelta64(1, "s")
-
-# The Tyre Compounds
-Compounds = ["SOFT", "MEDIUM", "HARD"]
-
-##Clean lap Time per Compound
-# For each compound using Quartile Analysis removing Outliers
-# Using Rolling 5 lap Times in order to normalize data and to be easier to analyze
-dftemp = pd.DataFrame()
-for compound in Compounds:
-    df_compound = laps[laps["Compound"] == compound]
-    Q1 = df_compound["lapinseconds"].quantile(0.25)
-    Q3 = df_compound["lapinseconds"].quantile(0.75)
-    IQR = Q3 - Q1
-    df_compound = df_compound[
-        ~(
-            (df_compound["lapinseconds"] < (Q1 - 1.5 * IQR))
-            | (df_compound["lapinseconds"] > (Q3 + 1.5 * IQR))
-        )
-    ]
-    df_compound = (
-        df_compound[["lapinseconds", "TyreLife"]]
-        .groupby(["TyreLife"])
-        .mean()
-        .reset_index(drop=False)
-    )
-    df_compound["RollingLapTime"] = (
-        df_compound["lapinseconds"]
-        .rolling(window=5, min_periods=1)
-        .mean()
-        .fillna(df_compound["lapinseconds"])
-    )
-    df_compound["Compound"] = compound
-    df_compound["Race"] = race.weekend.name
-    dftemp = pd.concat([dftemp, df_compound])
-    df = pd.concat([df, df_compound])
-
-##Visualizing Results
-pd.pivot_table(
-    dftemp, values="RollingLapTime", columns="Compound", index="TyreLife"
-).plot(title=race.weekend.name)
-i = i + 1
-# asdasf
+st.write("Results:", data)
---
+++ tyre_analysis.py
@@ -0,0 +1,185 @@
+import pandas as pd
+import numpy as np
+import fastf1
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit as st
+
+sns.set()
+
+
+# List of Teams ['Alpine F1 Team', 'Mercedes', 'AlphaTauri', 'Alfa Romeo',
+#        'Williams', 'Ferrari', 'Haas F1 Team', 'McLaren', 'Red Bull',
+#        'Aston Martin']
+# List of Drivers ['VER', 'HAM', 'BOT', 'LEC', 'GAS', 'RIC', 'NOR', 'SAI', 'ALO',
+#        'STR', 'PER', 'GIO', 'TSU', 'RAI', 'RUS', 'OCO', 'LAT', 'MSC',
+#        'MAZ', 'VET']
+
+
+def load_race_data():
+    df = pd.DataFrame()
+    count = 1
+    while count <= 22:
+        race = fastf1.get_session(2021, count, "R")
+        laps = race.load_laps(with_telemetry=True)
+        laps["Race"] = race.weekend.name
+        df = pd.concat([df, laps])
+        count += 1
+    return df
+
+
+def remove_wet_races(data):
+    wet_races = (
+        data[["Race", "Compound", "Driver"]]
+        .groupby(["Race", "Compound"])
+        .count()
+        .reset_index()
+    )
+    wet_races = pd.pivot_table(
+        wet_races, index="Race", columns="Compound", values="Driver"
+    ).reset_index()
+    wet_races = wet_races.fillna(0)
+    wet_races = wet_races[(wet_races["WET"] == 0) & (wet_races["INTERMEDIATE"] == 0)]
+    data = pd.merge(data, wet_races[["Race"]], on="Race")
+    data = data[data["Compound"] != "UNKNOWN"]
+    return data
+
+
+def add_difference_from_the_car_in_front(data):
+    # Sort Values by Following Car
+    data = data.sort_values(by=["Race", "Time"]).reset_index(drop=True)
+    ##Add column with the delta between 2 cars in the beginning of the lap
+    data["followingcar"] = data["Time"] - data["Time"].shift(1)
+    return data
+
+
+def filter_dataframe(data):
+    # Filter Dataframe where
+    # 1.there is no pit in or pit out in this lap
+    # 2.The track is Clear
+    # 3.there is no car ahead less than 1 second
+    # 4.Only Dry Tyres
+    # 5.Keeping only necessery Columns
+    data = data[
+        (data["PitOutTime"].isnull())
+        & (data["PitInTime"].isnull())
+        & (data["TrackStatus"] == "1")
+        & (data["followingcar"] > pd.Timedelta(1.1, unit="s"))
+    ][
+        [
+            "Race",
+            "LapTime",
+            "LapNumber",
+            "Compound",
+            "TyreLife",
+            "Team",
+            "Driver",
+        ]
+    ]
+    return data
+
+
+def add_tyre_time_difference(data):
+    # Creating a column with the laptime delta between the laps
+    data = data.sort_values(
+        by=["Race", "Driver", "LapNumber", "TyreLife", "Compound"]
+    ).reset_index(drop=True)
+    data["tyredelta"] = np.where(
+        (data["Driver"] == data["Driver"].shift(1))
+        & (data["Compound"] == data["Compound"].shift(1))
+        & (data["Race"] == data["Race"].shift(1)),
+        pd.to_timedelta(data["LapTime"] - data["LapTime"].shift(1)),
+        pd.NaT,
+    )
+    data["tyredelta"] = data["tyredelta"].fillna(9999 * 1000)
+    data["tyredelta"] = (data["tyredelta"] / 1000).astype("int")
+    return data
+
+
+def add_laptime_to_seconds(data):
+    # Calculating Lap Times in Seconds
+    data["lapinseconds"] = data["LapTime"] / np.timedelta64(1, "s")
+    return data
+
+
+def q1(x):
+    return x.quantile(0.25)
+
+
+def q3(x):
+    return x.quantile(0.75)
+
+
+def calculate_quartiles(data):
+    f = {"lapinseconds": [q1, q3]}
+    quartiles = (
+        data[["Race", "Compound", "lapinseconds"]]
+        .groupby(["Race", "Compound"])
+        .agg(f)
+        .reset_index()
+    )
+    quartiles.columns = ["Race", "Compound", "Q1", "Q3"]
+    return quartiles
+
+
+def clean_outlier_lap_times(data):
+    # Clean Lap Times by Compound and per Race using Quartiles
+    var = 1.5
+    quartiles = calculate_quartiles(data)
+    data = pd.merge(data, quartiles, on=["Race", "Compound"], how="left")
+    data["IQR"] = data["Q3"] - data["Q1"]
+    data = data[
+        ~(
+            (data["lapinseconds"] < (data["Q1"] - var * data["IQR"]))
+            | (data["lapinseconds"] > (data["Q3"] + var * data["IQR"]))
+        )
+    ]
+    data = data.drop(columns=["Q1", "Q3", "IQR"])
+    return data
+
+
+def remove_na_rows(data):
+    # dropping row where containing na
+    data = data.dropna()
+    return data
+
+
+def rolling_lap_times(data):
+    data = data.reset_index(drop=True)
+    data = data.sort_values(by=["Race", "Driver", "LapNumber", "Compound", "TyreLife"])
+    data_temp = (
+        (
+            data.groupby(["Race", "Driver", "LapNumber", "Compound", "TyreLife"])
+            .rolling(window=5, min_periods=1)
+            .mean()
+            .reset_index()
+        )
+        .fillna(data["lapinseconds"])
+        .rename(columns={"lapinseconds": "Rolling_lap_times"})
+    )
+    data = pd.merge(
+        data,
+        data_temp[
+            ["Race", "Driver", "LapNumber", "Compound", "TyreLife", "Rolling_lap_times"]
+        ],
+        on=["Race", "Driver", "LapNumber", "Compound", "TyreLife"],
+        how="left",
+        validate="1:1",
+    )
+    return data_temp
+
+
+def create_race_data():
+    data = load_race_data()
+    data = remove_wet_races(data)
+    data = add_difference_from_the_car_in_front(data)
+    data = filter_dataframe(data)
+    data = add_tyre_time_difference(data)
+    data = add_laptime_to_seconds(data)
+    data = clean_outlier_lap_times(data)
+    data = remove_na_rows(data)
+    data = rolling_lap_times(data)
+    return data
+
+
+data = create_race_data()
